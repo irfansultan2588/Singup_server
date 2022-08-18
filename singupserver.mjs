80b8686a4d790d42cce623c13d1a7215df902a28
irfan <irfansultan201@gmail.com>
2022-08-18 13:13:22 +0500

    minor change

--- singupserver.mjs
+++ singupserver.mjs
@@ -111,6 +111,10 @@ app.post("/login", (req, res) => {
     }
 })
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
